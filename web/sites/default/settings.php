710c30b554672498d10951f370e877015a34cc75
Alexandre Galiay <dev@alexandregaliay.com>
2022-01-24 23:20:21 +0100

    Update trusted host patterns config

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -748,6 +748,11 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * example.org, with all subdomains included.
  */
 
+$settings['trusted_host_patterns'] = [
+  '^localhost$'
+];
+
+
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
