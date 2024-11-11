98011be36d0dd35b8e9b5a90e64d4b71fb407981
Alexandre Galiay <dev@alexandregaliay.com>
2024-11-11 23:59:39 +0100

    Trying again dunno why

--- web/themes/custom/boilerplate/vite.config.js
+++ web/themes/custom/boilerplate/vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
   return {
+    base: mode === 'development' ? '/' : '/web/themes/custom/boilerplate/dist',
+    publicDir: '/web/themes/custom/boilerplate/dist',
     build: {
       manifest: true,
       rollupOptions: {
@@ -17,8 +19,14 @@ export default defineConfig(({ mode }) => {
       host: 'drupalboilerplate.test',
       strictPort: true,
       open: true,
+      hmr: {
+        host: 'drupalboilerplate.test',
+      },
       proxy: {
-        '/': 'http://drupalboilerplate.test',
+        '/': {
+          target: 'http://drupalboilerplate.test',
+          changeOrigin: true,
+        },
       },
     },
   };
